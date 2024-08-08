575aab5e7f82f14ff3df1ba83dd7734692653053
Tagert <mini.andrius@gmail.com>
2024-08-08 10:57:01 +0300

    :sparkles: feat(Hero): Added hero component and all kind of icons.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
+import "./globals.css";
+import React from "react";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Andrejus Portfolio",
-  description: "Minimalist first portfolio",
+  description: "First minimalistic portfolio",
 };
 
 export default function RootLayout({
@@ -15,8 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "@/components/Hero";
 
-export const Home = () => {
+const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
@@ -9,3 +9,5 @@ export const Home = () => {
     </main>
   );
 };
+
+export default Home;
